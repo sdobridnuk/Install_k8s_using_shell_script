e347a7a8c49e5b263936b531bb45f476830ef614
Digpal Parmar <77digpal@gmail.com>
2023-04-13 10:39:02 +0530

    Update Install_K8s.sh

--- Install_K8s.sh
+++ Install_K8s.sh
@@ -76,7 +76,7 @@ fi
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-get install -y kubelet=1.26.0-00 kubeadm=1.26.0-00 kubectl=1.26.0-00
 sudo apt-mark hold kubelet kubeadm kubectl
 
 echo "Installing of k8s component is Successfull"
